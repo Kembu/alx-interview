b65bb52575a44314b42b1f6ac563040e054591c4
kembu <allantoo05@gmail.com>
2023-07-13 20:20:40 +0000

    fixed pycodestyle

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,9 +5,9 @@ Minimum Operations
 
 
 def minOperations(n):
-      """
-    Calculates the fewest number of operations \
-        needed to achieve exactly n 'H' characters in the file.
+    """
+    Calculates the fewest number of operations needed to achieve\
+            exactly n 'H' characters in the file.
 
     Arguments:
     - n: An integer representing the desired number of 'H' characters.
@@ -15,18 +15,18 @@ def minOperations(n):
     Returns:
     - An integer representing the minimum number of operations needed.
 
-    If it is impossible to achieve the desired number\
-        of 'H' characters, the function returns 0.
+    If it is impossible to achieve the desired number of 'H'\
+            characters, the function returns 0.
     """
 
-    n = n
+    num = n
 
-    if (n < 2):
+    if num < 2:
         return 0
-    elif (n < 6 or n % 2 != 0):
-        return n
+    elif num < 6 or num % 2 != 0:
+        return num
     else:
-        x = n
-        while (x % 2 == 0 and x > 5):
+        x = num
+        while x % 2 == 0 and x > 5:
             x = x // 2
-        return ((n // x) + x)
+        return (num // x) + x
